ec01f5c4931099fa812fa68c957445067df54f5e
amrutakhot317 <amrutakhot2994@gmail.com>
2020-10-01 14:01:16 +0530

    third commit

--- src/component/footer-Component.js
+++ src/component/footer-Component.js
@@ -3,7 +3,7 @@ import React from "react";
 const Footer = () => {
   return (
     <>
-      <footer> All area is reserved </footer>
+      <footer> git hub All area is reserved </footer>
     </>
   );
 };
